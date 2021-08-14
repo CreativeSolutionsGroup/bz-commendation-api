06cf21e5d2adb51e4bb9e721c74d37866fa92b13
spbills <spencebills0@gmail.com>
2021-08-14 14:35:41 -0400

    Init serverless/lambda wrapper.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import commendationsRouter from "./routers/commendations";
+import serverless from "serverless-http"
 import bodyParser from "body-parser";
 
 dotenv.config();
@@ -14,3 +15,5 @@ commendationsRouter(app);
 app.listen(port, () => {
     console.log(`Server started on port ${port} 🚀🚀🚀`);
 })
+
+module.exports.handler = serverless(app)

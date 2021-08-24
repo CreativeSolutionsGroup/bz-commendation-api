9617991d046ca150aa70953f8b302d6cfffaae46
Alec Mathisen <Alec-WAM@users.noreply.github.com>
2021-08-24 17:24:44 -0400

    Add Access-Control-Allow-Origin

--- src/app.ts
+++ src/app.ts
@@ -20,6 +20,13 @@ const app = express();
 app.use(bodyParser.json());
 commendationsRouter(app);
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
 
 app.listen(port, () => {
     console.log(`Server started on port ${port} 🚀🚀🚀`);

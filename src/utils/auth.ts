f104ec26583e6df98dbc56fe41893cc42adef3a2
spbills <spencebills0@gmail.com>
2021-09-16 17:00:32 +0000

    Added better auth checking.

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,5 +1,6 @@
 import { Request, Response, RequestHandler } from "express";
 import Axios from "axios";
+import { existsInSheet } from "../controllers/users";
 
 const checkLoggedIn: RequestHandler = async (req: Request, res: Response, next) => {
     let bearer = req.headers.authorization;
@@ -7,6 +8,14 @@ const checkLoggedIn: RequestHandler = async (req: Request, res: Response, next) 
     try {
         let splitBearer = bearer.split(" ")[1];
         let tokenRes = await Axios.get(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${splitBearer}`);
+
+        const email = tokenRes["email"];
+        
+        const allowedToAccess = await existsInSheet(email);
+
+        if (!allowedToAccess) {
+            throw "does not exist in sheet";
+        }
 
         if (tokenRes.status !== 200) {
             throw "bad auth";

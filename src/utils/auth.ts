1bf0b2a17d6b6664501090924337ffa2205cd5ee
Alec Mathisen <Alec-WAM@users.noreply.github.com>
2021-10-16 17:44:21 -0400

    Real sheet list check for email

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -10,7 +10,7 @@ export const checkLoggedIn: RequestHandler = async (req: Request, res: Response,
         let splitBearer = bearer.split(" ")[1];
         let tokenRes = await Axios.get(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${splitBearer}`);
 
-        const email = tokenRes["email"];
+        const email = tokenRes.data["email"];
         
         const allowedToAccess = await existsInSheet(email);
 

db55b190b4b9a53543852a2da3e69e6455ee3eb4
SpBills <spencebills0@gmail.com>
2022-04-13 15:07:48 +0000

    SQL now contains commendations.

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,9 +1,9 @@
 import {
-  Column,
-  Entity,
-  ManyToOne,
-  OneToMany,
-  PrimaryGeneratedColumn,
+    Column,
+    Entity,
+    ManyToOne,
+    OneToMany,
+    PrimaryGeneratedColumn,
 } from "typeorm";
 import Commendation from "./commendation";
 import Role from "./role";
@@ -11,32 +11,32 @@ import Team from "./team";
 
 @Entity()
 class User {
-  @PrimaryGeneratedColumn()
-  id: string;
+    @PrimaryGeneratedColumn()
+    id: string;
 
-  @Column()
-  email: string;
+    @Column()
+    email: string;
 
-  @Column()
-  name: string;
+    @Column()
+    name: string;
 
-  @Column()
-  phone: string;
+    @Column()
+    phone: string;
 
-  @Column()
-  googleId: string;
+    @Column()
+    googleId: string;
 
-  @ManyToOne(() => Team)
-  team: Team;
+    @ManyToOne(() => Team)
+    team: Team;
 
-  @ManyToOne(() => Role)
-  role: Role;
+    @ManyToOne(() => Role)
+    role: Role;
 
-  @OneToMany(() => Commendation, comm => comm.toUser)
-  receivedCommendations: Commendation;
+    @OneToMany(() => Commendation, (comm) => comm.toUser)
+    receivedCommendations: Commendation;
 
-  @OneToMany(() => Commendation, comm => comm.fromUser)
-  sentCommendations: Commendation;
+    @OneToMany(() => Commendation, (comm) => comm.fromUser)
+    sentCommendations: Commendation;
 }
 
 export default User;
